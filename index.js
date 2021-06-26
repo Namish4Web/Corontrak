6299f15fe8144a10c19ec7ccde7f016bc3c1cfd7
Arup <abehera@vocera.com>
2021-06-26 21:19:08 +0530

    fixed listing countries issue

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
- function done() {		firebase.database().ref('-McnY6zYgiieeXBqwg9X/Country').once('value',   function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
+function done() {
+  firebase.database().ref('-McnY6zYgiieeXBqwg9X').once('value', function (snapshot) {
+    snapshot.forEach(function (childSnapshot) {
       var childKey = childSnapshot.key;
       var childData = childSnapshot.val();
-      document.getElementById("total").innerHTML = clildSnapshot.val();
-      // ...
+      document.getElementById("total").innerHTML = document.getElementById("total").innerHTML + childSnapshot.val();
     });
   });
- }
- function download() {
-  window.location.href="https://corontrak.web.app/Corontrak.apk";
-  }
-  
-  function solver() {
-  				window.location.href="https://corontrak.web.app/queries.html";
-  }
+}
+function download() {
+  window.location.href = "https://corontrak.web.app/Corontrak.apk";
+}
+
+function solver() {
+  window.location.href = "https://corontrak.web.app/queries.html";
+}
